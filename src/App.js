e07e45af0b3df9bcf206f49d7f08ec31074b7130
patriciagilberta <patriciagilberta@gmail.com>
2021-06-01 16:02:53 +0700

    Offline info dan manifest

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import CLient from './components/Clients';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Offline from './components/Offline';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+
+  function handleOfflineStatus() {
+    setOfflineStatus(!navigator.onLine);
+  }
 
   React.useEffect(function() {
     (async function() {
@@ -22,10 +28,25 @@ function App() {
       });
       const { nodes } = await response.json();
       setItems(nodes);
+
+      const script = document.createElement("script");
+      script.src = "/carousel.js";
+      script.async = false;
+      document.body.appendChild(script);
     })();
-  }, []);
+
+    handleOfflineStatus();
+    window.addEventListener('online', handleOfflineStatus);
+    window.addEventListener('offline', handleOfflineStatus);
+
+    return function(){
+      window.removeEventListener('online', handleOfflineStatus);
+      window.removeEventListener('offline', handleOfflineStatus);
+    }
+  }, [offlineStatus]);
   return (
     <>
+      {offlineStatus && <Offline/>}
     <Header/>
     <Hero/>
     <Browse/>
